fbe861ac7186c46ebb72bb0fcbe7a841912a6ca5
Matthew Stefanovic <mstefanovic@stetson.edu>
2023-01-14 00:44:43 -0500

    Added switch statement

--- notion.js
+++ notion.js
@@ -17,6 +17,33 @@ getTags();
 
 
 function createAssignmentPage(assignmentName, dueDate) {
+    let doOnID;
+    switch (doOn) {
+        case 'sunday':
+            doOnID = "4abf0d3f-c2df-4fb2-b927-0fa3ff3ef3fc";
+            break;
+        case 'monday':
+            doOnID = '0d9dd7d8-f5fc-4823-99a3-f553a70dbb54';
+            break;
+        case 'tuesday':
+            doOnID = "8cbb5e25-f78a-45fe-b883-c5c9983c7afe";
+            break;
+        case 'wednesday':
+            doOnID = "7ceaf8a0-5c20-4d86-87dc-5144c1ffdb6d";
+            break;
+        case 'thursday':
+            doOnID = "ccf13268-2efc-4bbf-937a-390b1f5a885d";
+            break;
+        case 'friday':
+            doOnID = "8ee50ea7-275e-4ec6-af32-004b9316a83e";
+            break;
+        case 'sauterday':
+            doOnID = "10389cf8-6a01-43ee-9d88-8336eab6c3ca";
+            break;
+        default:
+            doOnID = null;
+    
+
     notion.pages.create({
         parent: {
             database_id: process.env.NOTION_DATABASE_ID
